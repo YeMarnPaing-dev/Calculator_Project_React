40e5e25de505e524bf899706ca113c87fbcd5d22
Saw Ye Marn <yemarnpaing44@gmail.com>
2025-11-15 09:49:40 +0630

    effect

--- src/Hook.jsx
+++ src/Hook.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import './Hook.css'
+import React,{useState} from "react";
+import "./Hook.css";
 
 const Hook = () => {
-  return (
-    <div>Hook</div>
-  )
-}
+    const [posts,setPost] = useState([]);
 
-export default Hook
+const fetchData = async () => {
+  const data = await fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .catch(error => console.log(error.message));
+
+   setPost(data)
+     
+};
+
+fetchData();
+
+    
+  return (
+    <div>
+        <h1>Api Post Data</h1>
+        <hr />
+        {
+            posts.map((post,index)=>{
+                <li key={index}>{post.title}</li>
+            })
+         }       
+  </div>
+  )
+  
+};
+
+export default Hook;
